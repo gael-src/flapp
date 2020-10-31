f3c4d32924c7ac2b6ea216a4ba7ecbdaa792bf4a
gael-src <src.gael@gmail.com>
2020-10-31 15:31:53 +0100

    started routing home, about and services pages

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>flapp</title>
-    </head>
-    <body>
-        <h1>flapp</h1>
-        <p>home</p>
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <h1>flapp</h1>
+    <h2>{{$title}}</h2>
+    <p>home</p>
+@endsection
